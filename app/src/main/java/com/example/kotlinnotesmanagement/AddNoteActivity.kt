ea4c303a1cd53c3b7bb0626162ecff329e7880ce
Gonni <nicolas.cros.sn@gmail.com>
2024-02-26 16:14:29 +0100

    update visual for categories picking

--- app/src/main/java/com/example/kotlinnotesmanagement/AddNoteActivity.kt
+++ app/src/main/java/com/example/kotlinnotesmanagement/AddNoteActivity.kt
@@ -3,11 +3,15 @@ package com.example.kotlinnotesmanagement
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -21,7 +25,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
 import com.example.kotlinnotesmanagement.data.database.DatabaseHelper
@@ -96,7 +103,12 @@ class AddNoteActivity : ComponentActivity() {
                 BasicTextField(
                     value = TextFieldValue(text = selectedCategories.joinToString(", ")),
                     onValueChange = {},
-                    modifier = Modifier.menuAnchor()
+                    modifier = Modifier.menuAnchor().height(50.dp).width(500.dp).border(width = 1.dp, color = Color.Black),
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        imeAction = ImeAction.Done, //  n'affiche pas de clavier
+                    ),
+                    visualTransformation = VisualTransformation.None,
+                    readOnly = true
                 )
 
                 ExposedDropdownMenu(
